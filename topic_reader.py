ad33e78b90c592182cd35e87220fcfffd4869f2a
Arjen P. de Vries <arjen@acm.org>
2019-06-17 12:08:36 +0200

    Might work...

--- topic_reader.py
+++ topic_reader.py
@@ -1,70 +1,83 @@
 #!/usr/bin/env python3
 
-import re
-import subprocess
+import argparse
+import sys
+import pymonetdb
+import random
+import string
+from topic_reader import TopicReader
+import time
 
-class TopicReader:
-    
-    def __init__(self, topics_file_name):
-        self.filename = topics_file_name
-        self.file = open(self.filename)
-        self.topics = []
-        self._read_topics_file()
-        self._preprocess_titles()
+class SearchCollection:
+   
+    def getQueryTemplate(self):
+        queryTemplate =  """
+            WITH qterms AS (SELECT termid, docid, count FROM terms 
+                WHERE termid IN ({})), 
+                subscores AS (SELECT docs.collection_id, docs.id, len, term_tf.termid, 
+                term_tf.tf, df, (log((528155-df+0.5)/(df+0.5))*((term_tf.tf*(1.2+1)/
+                (term_tf.tf+1.2*(1-0.75+0.75*(len/188.33)))))) AS subscore 
+                FROM (SELECT termid, docid, count as tf FROM qterms) AS term_tf 
+                JOIN (SELECT docid FROM qterms 
+                    GROUP BY docid HAVING COUNT(distinct termid) = {}) 
+                    AS cdocs ON term_tf.docid = cdocs.docid 
+                JOIN docs ON term_tf.docid = docs.id 
+                JOIN dict ON term_tf.termid = dict.termid) 
+            SELECT scores.collection_id, score FROM (SELECT collection_id, sum(subscore) AS score 
+                FROM subscores GROUP BY collection_id) AS scores JOIN docs ON 
+                scores.collection_id=docs.collection_id ORDER BY score DESC;
+        """
+        return queryTemplate
 
-    def _read_topics_file(self):
-        while True:
-            line = self.file.readline()
-            if not line:
-                break
-            
-            while line.strip() != '<top>':
-                line = self.file.readline()
-            
-            while not line.startswith('<num>'):
-                line = self.file.readline()
-            topic_no = int(re.search('Number: (\d+)', line.strip()).group(1))
-            
-            while not line.startswith('<title>'):
-                line = self.file.readline()
-            # Core18 specific - fix:
-            line = self.file.readline()
-            topic_title = line.strip()
-            # topic_title = line.strip()[8:]
-            
-            line = self.file.readline().strip()
-            while not line.startswith('<desc>'):
-                topic_title += line
-                line = self.file.readline().strip()
-            
-            topic_desc = ""
-            line = self.file.readline().strip()            
-            while not line.startswith('<narr>'):
-                topic_desc += line
-                line = self.file.readline().strip()
-            
-            topic_nar = ""
-            line = self.file.readline().strip()
-            
-            while line != '</top>':
-                topic_nar += line
-                line = self.file.readline().strip()
-                
-            topic = {   
-                        'number' : topic_no,
-                        'title' : topic_title,
-                        'description' : topic_desc,
-                        'narrative' :  topic_nar 
-                    }
-            self.topics.append(topic)
-    
-    def _preprocess_titles(self):
-        for i, topic in enumerate(self.topics):
-            title = topic['title']
-            process = subprocess.Popen("""./olddog/target/appassembler/bin/nl.ru.preprocess.ProcessQuery {}""".format(title).split(), stdout=subprocess.PIPE)
-            stdout = process.communicate()[0].decode("utf-8").strip()
-            self.topics[i]['title'] = stdout
+    def search(self):
+        topics = self.topicReader.get_topics()
+        ofile = open(self.args.output, 'w+')
+        print("SCORING TOPICS")
+        for topic in topics:
+            query_terms = topic['title'].split(" ")
+            ids = [] 
+            for qterm in query_terms:
+                self.cursor.execute("SELECT termid FROM dict WHERE dict.term = '{}'".format(qterm))
+                term_id = self.cursor.fetchone()
+                if term_id:
+                    ids.append(str(term_id[0]))
+            term_ids = ", ".join(ids)
+            sql_query = self.getQueryTemplate().format(term_ids, len(ids))
+            self.cursor.execute(sql_query)
+            output = self.cursor.fetchall()
+            for rank, row in enumerate(output):
+                collection_id, score = row
+                ofile.write("{} Q0 {} {} {} olddog\n".format(topic['number'], collection_id, rank+1, score))
 
+    def getConnectionCursor(self):
+        print("CREATE DATABASE") 
+        dbname = 'core18'
+        
+        connection = None 
+        attempt = 0
+        while connection is None or attempt > 20:
+            print("CREATE CONNECTION")
+            try:
+                connection = pymonetdb.connect(username='monetdb',
+                                               password='monetdb',
+                                               hostname='localhost', 
+                                               database=dbname)
+            except:
+                attempt += 1
+                time.sleep(5) 
 
-    def get_topics(self):
-        return self.topics
+        cursor = connection.cursor()
+        return cursor 
+
+    def __init__(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--filename', required=True, help='Topics file')
+        parser.add_argument('--output', required=True, help='filename for output')
+        self.args = parser.parse_args()
+        self.cursor = self.getConnectionCursor()
+        self.topicReader = TopicReader(self.args.filename)  
+        self.search() 
+        
+if __name__ == '__main__':
+    SearchCollection()      
+ 
