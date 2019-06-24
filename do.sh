94e5b8deb891d9de98d7d86ed56902b0387b4837
Arjen P. de Vries <arjen@acm.org>
2019-06-24 20:54:30 +0200

    Added an indexing step - this runs everything

--- do.sh
+++ do.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 pushd /export/data/ir/jig
 
+echo Indexing collections
+python3 run.py prepare \
+  --repo osirrc2019/olddog \
+  --collections robust04=/export/data/ir/robust04=trectext core18=/export/data/ir/WashingtonPost.v2/data=json
+
 echo Robust04 \(conjunctive\)
 python3 run.py search \
   --repo osirrc2019/olddog \
